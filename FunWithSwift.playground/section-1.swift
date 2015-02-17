c45b632cc31e4be0c62b75ccf6aafed33065e54d
AnYuan <donganyuan1010@gmail.com>
2015-02-17 11:04:47 +0800

    currying

--- FunWithSwift.playground/section-1.swift
+++ FunWithSwift.playground/section-1.swift
@@ -113,6 +113,89 @@ println(buildIndex(words))
 
 
 
+//Patial application which process of "fixing" one or more arguments of a function to create a new function
+//and currying
+import Foundation
+
+let data = "5,7;3,4;55,6"
+println(data.componentsSeparatedByString(";"))
+
+//func createSplitter(separator: String) -> (String -> [String]) {
+//    func split(source: String) -> [String] {
+//        return source.componentsSeparatedByString(separator)
+//    }
+//    return split
+//}
+//let commaSplitter = createSplitter(",")
+//println(commaSplitter(data))
+//
+//
+//let semiColonSpliter = createSplitter(";")
+//println(semiColonSpliter(data))
+
+
+
+//currying
+func createSplitter(separator: String)(source: String) -> [String] {
+    return source.componentsSeparatedByString(separator)
+}
+
+let commaSplitter = createSplitter(",")
+println(commaSplitter(source:data))
+
+
+let semiColonSpliter = createSplitter(";")
+println(semiColonSpliter(source:data))
+
+
+
+
+func addNumbers(one: Int, two: Int, three: Int) -> Int
+{
+    return one + two + three
+}
+
+let sum = addNumbers(2,5, 4)
+println(sum)
+
+
+func curryAddNumbers(one: Int)(two: Int)(three: Int) -> Int
+{
+    return one + two + three
+}
+
+let stepOne = curryAddNumbers(2)
+let stepTwo = stepOne(two: 5)
+let result = stepTwo(three: 4);
+println(result)
+
+let result2 = curryAddNumbers(2)(two: 5)(three: 4)
+println(result2)
+
+func curryAddNumbers2(one: Int, two: Int)(three: Int) -> Int
+{
+    return one + two + three
+}
+
+
+let result3 = curryAddNumbers2(2, 5)(three: 4)
+println(result3)
+
+//practical currying
+let text = "Swift"
+let paddedText = text.stringByPaddingToLength(10, withString: ".", startingAtIndex: 0)
+println(paddedText)
+
+func curriedPadding(startingAtIndex: Int, withString: String)(source: String, length: Int) -> String
+{
+    return source.stringByPaddingToLength(length, withString: withString, startingAtIndex: startingAtIndex)
+}
+
+let dotPadding = curriedPadding(0, ".")
+
+let dotPadded = dotPadding(source:"Curry!", length: 10)
+println(dotPadded)
+
 
 
 

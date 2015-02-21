f4b8b67ca788899ca8344a6a7e8371a41366f6e0
AnYuan <donganyuan1010@gmail.com>
2015-02-21 17:52:38 +0800

    add SwiftReversi

---
+++ SwiftReversi/SwiftReversi/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SwiftReversi
+//
+//  Created by Colin Eberhardt on 07/06/2014.
+//  Copyright (c) 2014 razeware. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+                            
+  var window: UIWindow?
+
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: NSDictionary?) -> Bool {
+    return true
+  }
+}
+
---
+++ SwiftReversi/SwiftReversi/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftReversi
+//
+//  Created by Colin Eberhardt on 07/06/2014.
+//  Copyright (c) 2014 razeware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  @IBOutlet var blackScore : UILabel!
+  @IBOutlet var whiteScore : UILabel!
+  @IBOutlet var restartButton : UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+

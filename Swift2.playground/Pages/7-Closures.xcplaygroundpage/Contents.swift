7e04f707f2da8968c5715bef48de8142b653f660
AnYuan <donganyuan1010@gmail.com>
2015-06-29 22:23:22 +0800

    add closures

--- Swift2.playground/Pages/7-Closures.xcplaygroundpage/Contents.swift
+++ Swift2.playground/Pages/7-Closures.xcplaygroundpage/Contents.swift
@@ -28,3 +28,84 @@ reversed = names.sort({(s1: String, s2: String) -> Bool in
 reversed = names.sort({s1, s2 in return s1 > s2})
 
 //: Implicit Returns from single-expression closures
+reversed = names.sort({s1, s2 in s1 > s2})
+
+//: Shorthand Argument Names
+reversed = names.sort({$0 > $1})
+
+//: Operator Functions
+reversed = names.sort(>)
+
+//: Trailing Closures
+func someFunctionThatTakesAClosure(closure:() -> Void) {
+    //function body goes here
+}
+
+//here's how you call this function without using a trailing closure:
+
+someFunctionThatTakesAClosure({
+    //closure's body goes here
+})
+
+
+//here's how you call this function with a trailing closure instead:
+someFunctionThatTakesAClosure() {
+    //trailing closure's body goes here
+}
+
+reversed = names.sort(){
+    $0 > $1
+}
+
+let digitNames = [0:"zero", 1:"one", 2:"two", 3:"three", 4:"four",
+    5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"]
+let numbers = [16,58,510]
+let strings = numbers.map {
+    (var number) -> String in
+    var output = ""
+    while number > 0 {
+        output = digitNames[numbers % 10]! + output
+        number /= 10
+    }
+    return output
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

aca336cb02348d66f6f51f3cc7c9550041a96b48
AnYuan <donganyuan1010@gmail.com>
2015-06-30 16:26:10 +0800

    fix miss type

--- Swift2.playground/Pages/7-Closures.xcplaygroundpage/Contents.swift
+++ Swift2.playground/Pages/7-Closures.xcplaygroundpage/Contents.swift
@@ -57,6 +57,10 @@ reversed = names.sort(){
     $0 > $1
 }
 
+reversed = names.sort{
+    $0 > $1
+}
+
 let digitNames = [0:"zero", 1:"one", 2:"two", 3:"three", 4:"four",
     5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"]
 let numbers = [16,58,510]

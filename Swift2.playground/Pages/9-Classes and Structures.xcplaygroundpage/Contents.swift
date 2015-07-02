89735f7a35b24a729435e7638322c7ef73794f2b
AnYuan <donganyuan1010@gmail.com>
2015-07-02 22:40:19 +0800

    add code to 9th chapter

--- Swift2.playground/Pages/9-Classes and Structures.xcplaygroundpage/Contents.swift
+++ Swift2.playground/Pages/9-Classes and Structures.xcplaygroundpage/Contents.swift
@@ -1,7 +1,53 @@
-//: [Previous](@previous)
 
 import Foundation
 
 var str = "Hello, playground"
 
-//: [Next](@next)
+/*: 
+### Classes VS Structures
+#### Both can:
+* Define properties to store values
+* Define methods to provide functionality
+* Define subscripts to provide access to their values using subscript syntax
+* Define initializers to set up their initial state
+* Be extended to expand their functionality beyond a default implementation
+* Comform to protocols to provide standard functionality of a certain kind
+
+#### Classes have additional capabilities that Structures DO NOT:
+* Inheritance enables one class to inherit the characteristics of another.
+* Type casting enables you to check and interpret the type of a class instance at runtime.
+* Deinitializers enable an instance of a class to free up any resources it has assigned.
+* Reference counting allows more than one reference to a class instance.
+*/
+
+
+//: Definition Syntax
+class SomeClass {
+    
+}
+
+struct SomeStructure {
+    
+}
+
+struct Resolution {
+    var width = 0
+    var height = 0
+}
+
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+
+//:Class and Structure Instance
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
+
+//:Accessing Properties
+print(someResolution.height)
+print(someVideoMode.resolution.width)
+someVideoMode.resolution.width = 1280
+

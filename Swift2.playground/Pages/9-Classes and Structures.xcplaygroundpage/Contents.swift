f2a5cfe0e6f92aa97d53bcc99dad55a0ae09afd5
AnYuan <donganyuan1010@gmail.com>
2015-07-10 21:53:45 +0800

    class and structures chapter

--- Swift2.playground/Pages/9-Classes and Structures.xcplaygroundpage/Contents.swift
+++ Swift2.playground/Pages/9-Classes and Structures.xcplaygroundpage/Contents.swift
@@ -51,3 +51,92 @@ print(someResolution.height)
 print(someVideoMode.resolution.width)
 someVideoMode.resolution.width = 1280
 
+//:Memberwise Initializers for Structure Type
+let vga = Resolution(width: 640, height: 480)
+
+//:Structures and Enumerations are Value Types
+//Struct
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+
+cinema.width = 2048
+hd.width
+//enum
+enum CompassPoint {
+    case North, South, East, West
+}
+var currentDirection = CompassPoint.West
+let rememberedDirection = currentDirection
+currentDirection = .East
+if rememberedDirection == .West {
+    print("west")
+}
+
+//:Classes are Reference Types
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+
+tenEighty.frameRate
+
+
+/*:
+#### Identity Operators:
+* Identical to (===)
+* Not identical to (!==)
+* "Identical to" means that two constants or variables of class type refer to exactly the same class instance.
+* "Equal to" means that two instances are considered "equal" or "equivalent" in value, for some appropriate meaning of "equal", as defined by the type's designer.
+*/
+if tenEighty === alsoTenEighty {
+    print("refer to the same VideoMode instance")
+}
+
+
+/*:
+#### Choosing Structures
+* The structure's primary purpose is to encapsulate a few relatively simple data values.
+* It is reasonable to expect that the encapsulated values will be copied rather than referenced when you assign or pass around an instance of that structure.
+* Any properties stored by the structure are themselves value types, which would also be expected to be copied rather than referenced.
+* The structure does not need to inherit properties or behavior from another existing type.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

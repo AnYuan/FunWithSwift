3421822f5b3cadb271897a2386e91d39f1bf8dc7
AnYuan <donganyuan1010@gmail.com>
2016-06-19 23:12:57 +0800

    init networking code

--- SwiftTalkSourceCode.playground/Contents.swift
+++ SwiftTalkSourceCode.playground/Contents.swift
@@ -1,5 +1,34 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCPlayground
 
-var str = "Hello, playground"
+struct Epsiode {}
+
+struct Media {}
+
+struct Resource<A> {
+    let url: URL
+    let parse: (Data) -> A?
+}
+
+let url = URL(string: "")!
+
+let episodeesResource = Resource<Data>(url: url) {
+    data in
+    return data
+}
+
+final class WebService {
+    let session = URLSession.shared()
+    
+    func loadEpisodes(completion: ([Epsiode]?) -> ()) {
+        
+    }
+    
+    func loadMedia(episode: Epsiode, completion: (Media?) -> ()) {
+        
+    }
+}
+
+

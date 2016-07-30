870674e183e67ce316ec6460158713e77003ed12
AnYuan <donganyuan1010@gmail.com>
2016-07-30 18:13:20 +0800

    A Simple Regular Expression Matcher

--- Swift.playground/Pages/31-Strings.xcplaygroundpage/Contents.swift
+++ Swift.playground/Pages/31-Strings.xcplaygroundpage/Contents.swift
@@ -77,6 +77,87 @@ if let idx = greet.characters.index(of: "!") {
 
 greet
 
+//A Simple Regular Expression Matcher
+
+//A simple regular expression type, supporting ^ and $ anchors,
+//and matching with . and *
+public struct Regex {
+    private let regexp: String
+    //construct from a regular expression String
+    public init(_ regexp: String) {
+        self.regexp = regexp
+    }
+}
+
+extension Regex {
+    //returns true if the string argument matches the expression
+    public func match(_ text: String) -> Bool {
+        
+        //if the regex starts with ^, then it can only match the 
+        //start of the input
+        if regexp.characters.first == "^" {
+            return Regex.matchHere(regexp.characters.dropFirst(),text.characters)
+        }
+        
+        //otherwise, search for a match at every point in the input
+        //until one is found
+        var idx = text.startIndex
+        while true {
+            if Regex.matchHere(regexp.characters, text.characters.suffix(from: idx)) {
+                return true
+            }
+            guard idx != text.endIndex else { break }
+            idx = text.index(after: idx)
+        }
+        return false
+    }
+}
+
+extension Regex {
+    //match a regular expression string at the beginning of text.
+    private static func matchHere(_ regexp: String.CharacterView, _ text: String.CharacterView) -> Bool {
+        //empty regexprs match everything
+        if regexp.isEmpty {
+            return true
+        }
+        //any character followed by * requires a call to matchStar
+        if let c = regexp.first , regexp.dropFirst().first == "*" {
+            return matchStar(c, regexp.dropFirst(2), text)
+        }
+        
+        //if this is the last regex acharacter and it's $, then
+        //it's a match iff the remaining text is also empty
+        if regexp.first == "$" && regexp.dropFirst().isEmpty {
+            return text.isEmpty
+        }
+        
+        //if one character matches, drop one from the input
+        //and the regex and keep matching
+        if let tc = text.first, let rc = regexp.first , rc == "." || tc == rc {
+            return matchHere(regexp.dropFirst(), text.dropFirst())
+        }
+        
+        //if none of the above, no match
+        return false
+    }
+    
+    //search for zero or more 'c''s at beginning of text, followed by the remainder of the regular expression.
+    private static func matchStar(_ c: Character, _ regexp: String.CharacterView, _ text: String.CharacterView) -> Bool {
+        var idx = text.startIndex
+        while true {
+            if matchHere(regexp, text.suffix(from: idx)) {
+                return true
+            }
+            if idx == text.endIndex || (text[idx] != c && c != ".") {
+                return false
+            }
+            idx = text.index(after: idx)
+        }
+    }
+}
+
+
+Regex("^h..lo*!$").match("hellooooooo!")
 
 
 

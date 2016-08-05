b651450abb1382699f360201c9bc10f3b7b0c3d1
AnYuan <donganyuan1010@gmail.com>
2016-08-05 15:39:59 +0800

    add Result

--- Swift.playground/Pages/26-Result.xcplaygroundpage/Contents.swift
+++ Swift.playground/Pages/26-Result.xcplaygroundpage/Contents.swift
@@ -11,4 +11,43 @@ enum Result<T> {
 }
 
 
-
+extension Result {
+    func map<U>(f: (T) -> U) -> Result<U> {
+        switch self {
+        case .success(let t):
+            return .success(f(t))
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+    func flatMap<U>(f: (T) -> Result<U>) -> Result<U> {
+        switch self {
+        case .success(let t):
+            return f(t)
+        case .failure(let error):
+            return .failure(error)
+        }
+    }
+    
+    //return the value if it's a .success or throw the error if it's a .failure
+    func resolve() throws -> T {
+        switch self {
+        case .success(let value):
+            return value
+        case .failure(let error):
+            throw error
+        }
+    }
+    
+    //construct a .success if the expresion returns a value
+    //or a .failure if it throws
+    init(_ throwingExpression: @noescape (Void) throws -> T) {
+        do {
+            let value = try throwingExpression()
+            self = .success(value)
+        } catch {
+            self = .failure(error)
+        }
+    }
+}

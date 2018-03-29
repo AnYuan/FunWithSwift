0deacf30dd1bb685efeb6a3c2ce64a8907986eed
anyuan dong <donganyuan@didichuxing.com>
2018-03-29 22:28:49 +0800

    finish collection

--- Swift.playground/Pages/Built-In Collections.xcplaygroundpage/Contents.swift
+++ Swift.playground/Pages/Built-In Collections.xcplaygroundpage/Contents.swift
@@ -45,7 +45,7 @@ extension Array {
 }
 
 example(of: "accumulate") {
-    [1,2,3,4].accumulate(0, +)
+    print([1,2,3,4].accumulate(0, +))
 }
 
 
@@ -110,5 +110,136 @@ example(of: "slices") {
 }
 
 
+//Dictionary
+enum Setting {
+    case text(String)
+    case int(Int)
+    case bool(Bool)
+}
+
+example(of: "uniquingKeysWith") {
+    
+    let defaultSettings: [String: Setting] = [
+        "Airplane Mode" : .bool(false),
+        "Name": .text("My iPhone")
+    ]
+    
+    var settings = defaultSettings
+    let overriddenSettings: [String: Setting] = ["Name": .text("Jane's iPhone")]
+    settings.merge(overriddenSettings, uniquingKeysWith: {$1})
+    print(settings)
+    
+    let settingsAsStrings = settings.mapValues {
+        setting -> String in
+        switch setting {
+        case .text(let text):
+            return text
+        case .int(let number):
+            return String(number)
+        case .bool(let value):
+            return String(value)
+        }
+    }
+    print(settingsAsStrings)
+}
 
 
+extension Sequence where Element: Hashable {
+    var frequencies:[Element: Int] {
+        let frequencyPairs = self.map { ($0, 1)}
+        return Dictionary(frequencyPairs, uniquingKeysWith:+)
+    }
+}
+example(of: "frequencies") {
+    let frequencies = "hello".frequencies
+    print(frequencies.filter {$0.value > 1})
+}
+
+example(of: "map values") {
+}
+
+
+//set
+example(of: "set") {
+    let naturals:Set = [1,2,3,2]
+    naturals
+    naturals.contains(3)
+    naturals.contains(0)
+}
+
+//set
+example(of: "set algebra") {
+    let iPods:Set = ["iPod touch", "iPod nano", "iPod mini", "iPod shuffle", "iPod Classic"]
+    let discontinuedIPods:Set = ["iPod mini", "iPod Classic", "iPod nano", "iPod shuffle"]
+    let currentIpods = iPods.subtracting(discontinuedIPods)
+    print(currentIpods)
+    
+    let touchscreen:Set = ["iPhone", "iPad", "iPod touch", "iPod nano"]
+    let iPodsWithTouch = iPods.intersection(touchscreen)
+    print(iPodsWithTouch)
+    
+    var discountinued:Set = ["iBook", "PowerBook", "Power Mac"]
+    discountinued.formUnion(discontinuedIPods)
+    print(discountinued)
+}
+
+example(of: "index set") {
+    var indices = IndexSet()
+    indices.insert(integersIn: 1..<5)
+    indices.insert(integersIn: 11..<15)
+    print(indices)
+    let evenIndices = indices.filter {$0 % 2 == 0}
+    print(evenIndices)
+}
+
+extension Sequence where Element:Hashable {
+    func unique() -> [Element] {
+        var seen: Set<Element> = []
+        return filter {
+            element in
+            if seen.contains(element) {
+                return false
+            } else {
+                seen.insert(element)
+                return true
+            }
+        }
+    }
+}
+
+example(of: "Using Sets Inside Closures") {
+    print([1,2,3,12,1,3,4,5,6,4,6].unique())
+}
+
+
+/*
+ Half-open and closed ranges both have a place:
+ * Only a half-open range can represent an empty interval(when the lower and upper bounds are equal, as in 5..<5)
+ * Only a closed range can contain the maximum value its element type can represent (e.g. 0...Int.max). A half-open range always requires at least one representable value that's greater than the highest value in the range.
+ */
+example(of: "Ranges") {
+    let singleDigitNumbers = 0..<10
+    type(of: singleDigitNumbers)
+    Array(singleDigitNumbers)
+    let lowercaseLetters = Character("a")...Character("z")
+    let fromZero = 0...
+    let upToZ = ..<Character("z")
+    print(singleDigitNumbers.contains(9))
+    print(lowercaseLetters.overlaps("c"..<"f"))
+}
+
+example(of: "partial ranges") {
+    let fromA: PartialRangeFrom<Character> = Character("a")...
+    let throughZ:PartialRangeThrough<Character> = ...Character("z")
+    let upto10:PartialRangeUpTo<Int> = ..<10
+    let fromFive:CountablePartialRangeFrom<Int> = 5...
+}
+
+example(of: "range expressions") {
+    let arr = [1,2,3,4]
+    print(arr[2...])
+    print(arr[..<1])
+    print(arr[1...2])
+    print(arr[...])
+    print(type(of: arr))
+}

2503d635a3d8728ffd3d8551c37575aec48b78f0
AnYuan <donganyuan1010@gmail.com>
2018-05-06 17:05:27 +0800

    deleting unnecessary code

--- SwfitCodable.playground/Pages/1. First Flight.xcplaygroundpage/Contents.swift
+++ SwfitCodable.playground/Pages/1. First Flight.xcplaygroundpage/Contents.swift
@@ -14,28 +14,6 @@ struct Plane: Codable {
     var manufacturer: String
     var model: String
     var seats: Int
-    
-    private enum CodingKeys: String, CodingKey {
-        case manufacturer
-        case model
-        case seats
-    }
-    
-    //Decodable
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.manufacturer = try container.decode(String.self, forKey: .manufacturer)
-        self.model = try container.decode(String.self, forKey: .model)
-        self.seats = try container.decode(Int.self, forKey: .seats)
-    }
-    
-    //Encodable
-    func encode(to encoder: Encoder) throws {
-        var container = try encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.manufacturer, forKey: .manufacturer)
-        try container.encode(self.model, forKey: .model)
-        try container.encode(self.seats, forKey: .seats)
-    }
 }
 
 let decoder = JSONDecoder()

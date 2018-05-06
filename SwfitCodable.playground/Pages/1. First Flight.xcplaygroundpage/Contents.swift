d395a28d9774020451bdb3f50f4a30ba0ca51b3d
AnYuan <donganyuan1010@gmail.com>
2018-05-06 17:16:39 +0800

    formation

--- SwfitCodable.playground/Pages/1. First Flight.xcplaygroundpage/Contents.swift
+++ SwfitCodable.playground/Pages/1. First Flight.xcplaygroundpage/Contents.swift
@@ -27,3 +27,42 @@ let encoder = JSONEncoder()
 let reencodedJSON = try! encoder.encode(plane)
 print(String(data: reencodedJSON, encoding: .utf8)!)
 
+let json1 = """
+[{
+"manufacturer": "Cessna",
+"model": "172 Skyhawk",
+"seats": 4,
+},
+{
+"manufacturer": "Piper",
+"model": "PA-28 Cherokee",
+"seats": 4,
+}]
+""".data(using: .utf8)!
+
+let planes = try! decoder.decode([Plane].self, from: json1)
+print(planes)
+
+let json2 = """
+{
+"planes": [{
+    "manufacturer": "Cessna",
+    "model": "172 Skyhawk",
+    "seats": 4,
+    },
+    {
+    "manufacturer": "Piper",
+    "model": "PA-28 Cherokee",
+    "seats": 4,
+    }]
+}
+""".data(using: .utf8)!
+
+let keyedPlanes = try! decoder.decode([String:[Plane]].self, from: json2)
+print(keyedPlanes["planes"])
+
+struct Fleet: Decodable {
+    var planes: [Plane]
+}
+let fleet = try! decoder.decode(Fleet.self, from: json2)
+print(fleet.planes)

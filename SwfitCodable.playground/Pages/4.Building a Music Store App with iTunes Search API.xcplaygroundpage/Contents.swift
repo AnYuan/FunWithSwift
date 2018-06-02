51908cad6ec2c526f63a34370b442014f5142a1f
AnYuan <donganyuan1010@gmail.com>
2018-06-02 18:44:00 +0800

    Chapter 4- Building a Music Store App with iTunes Search API

---
+++ SwfitCodable.playground/Pages/4.Building a Music Store App with iTunes Search API.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*
+import Foundation
+import UIKit
+import PlaygroundSupport
+
+let viewController = MasterViewController()
+let navigationController = UINavigationController(rootViewController: viewController)
+
+viewController.search(for: Music.self, with: "王力宏")
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+PlaygroundPage.current.liveView = navigationController
+*/
+
+

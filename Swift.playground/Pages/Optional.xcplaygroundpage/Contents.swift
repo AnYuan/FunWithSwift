65a37a501e38343799baef7c325ccbd786ae4bb8
Anyuan Dong <donganyuan1010@gmail.com>
2019-04-04 14:52:40 +0800

    update Optional

--- Swift.playground/Pages/Optional.xcplaygroundpage/Contents.swift
+++ Swift.playground/Pages/Optional.xcplaygroundpage/Contents.swift
@@ -1,23 +1,64 @@
 import Foundation
 
-infix operator !!
-func !! <T>(wrapped:T?, failureText:@autoclosure() -> String) -> T {
-    if let x = wrapped { return x }
-    fatalError(failureText())
+
+var funcations: [() -> Int] = []
+for i in 1...3 {
+    funcations.append { i }
 }
-
-let s = "foo"
-let i = Int(s) !! "Expecting integer, got \"\(s)\""
-
-
-infix operator !?
-func !?<T:ExpressibleByIntegerLiteral>(wrapped:T?, failureText:@autoclosure() -> String) -> T {
-    assert(wrapped != nil, failureText)
-    return wrapped ?? 0
+for f in funcations {
+    print("\(f())")
 }
 
 
-let ss = "20"
-let ii = Int(s) !? "Expecting integer, got \"\(s)\""
+// Doubly Nested Optionals
 
+let stringNumbers = ["1", "2", "three"]
+let maybeInts = stringNumbers.map { Int($0) }
+
+//var iterator = maybeInts.makeIterator()
+//while let maybeInt = iterator.next() {
+//    print(maybeInt)
+//}
+
+
+for case let i? in maybeInts {
+    print(i)
+}
+
+for case nil in maybeInts {
+    print("no value")
+}
+
+for case let .some(i) in maybeInts {
+    print(i)
+}
+
+
+let j = 5
+if case 0..<10 = j {
+    print("\(j) within range")
+}
+
+
+struct Pattern {
+    let s: String
+    init(_ s: String) { self.s = s }
+}
+
+func ~= (pattern: Pattern, value: String) -> Bool {
+    return value.range(of: pattern.s) != nil
+}
+
+
+let s = "Taylor Swift"
+if case Pattern("Swift") = s {
+    print("\(String(reflecting: s)) contains \"Swift\"")
+}
+
+let numbers = ["1", "2", "3", "foo"]
+print(numbers.compactMap{Int($0)}.reduce(0, +))
+
+
+let ns = [[1], [2,3], [4]]
+print(ns.flatMap { $0 })
 
